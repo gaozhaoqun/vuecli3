7f381b7f875685ddbb9d26e39a69d0c5b092ab50
gaozhaoqun <2855521@qq.com>
2019-01-08 18:11:15 +0800

    router-view组件是负责显示路由里面内容的

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <HelloWorld :msgSon="msgFahter"/>
+    </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld";
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+    name: "home",
+    components: {
+        HelloWorld
+    },
+    data() {
+        return {
+            msgFahter: "Welcome to Vue-cli3"
+        };
+    }
+};
 </script>
