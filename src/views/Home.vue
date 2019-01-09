d98b618f5811329fe9dbb0f56cc43cf76b7bf156
gaozhaoqun <2855521@qq.com>
2019-01-09 11:38:44 +0800

    试试 vscode提交

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,7 +24,7 @@ export default {
     data() {
         return {
             msgFahter: "Welcome to Vue-cli3",
-            title: "    ",
+            title: "",
             sonToFather: ""
         };
     },
